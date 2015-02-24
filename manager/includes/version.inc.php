2b495c53d6d87a4d3060ed97dd9bfefa91c36724
Richard Smith <richard@uxello.com>
2015-02-24 12:05:17 +0000

    custom version

--- manager/includes/version.inc.php
+++ manager/includes/version.inc.php
@@ -3,4 +3,4 @@ $modx_version = '1.0.15';           // Current version number
 $modx_release_date = 'Nov 06, 2014'; // Date of release
 $modx_branch = 'Evolution';        // Codebase name
 $modx_full_appname = 'MODX '.$modx_branch.' '.$modx_version.' ('.$modx_release_date.')';
-$modx_full_appname ='UXELLO (Feb 16, 2015) '.$modx_full_appname;
+$modx_full_appname ='UX001 '.$modx_full_appname;
