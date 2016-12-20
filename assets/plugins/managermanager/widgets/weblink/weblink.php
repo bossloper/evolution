27b9c61fd4132cca062d15e1334d30b2fb121a35
Mr B <bossloper@users.noreply.github.com>
2016-12-20 18:43:49 +0000

    Update weblink.php

--- assets/plugins/managermanager/widgets/weblink/weblink.php
+++ assets/plugins/managermanager/widgets/weblink/weblink.php
@@ -41,27 +41,33 @@ function mm_widget_weblink($roles='', $templates='') {
 					}
 				}
 				if (typeof(BrowseFileServer) != 'function') {	
-					function BrowseFileServer(ctrl) {
+					function BrowseFileServer(ctrl) { 
 						lastFileCtrl = ctrl;
-						var w = screen.width * 0.7;
-						var h = screen.height * 0.7;
-						OpenServerBrowser('".$base_url."manager/media/browser/mcpuk/browser.html?Type=files&Connector=".$base_url."manager/media/browser/mcpuk/connectors/php/connector.php&ServerPath=".$base_url."', w, h);
+						var w = screen.width * 0.5;
+						var h = screen.height * 0.5;
+						OpenServerBrowser('".$base_url."manager/media/browser/mcpuk/browser.php?Type=files', w, h);	
 					}
 				}
-				if (typeof(SetUrl) != 'function') {			
-					function SetUrl(url, width, height, alt){
-						if(lastFileCtrl) {
-							var c = document.mutate[lastFileCtrl];
-							if(c) c.value = url;
-							lastFileCtrl = '';
-						} else if(lastImageCtrl) {
-							var c = document.mutate[lastImageCtrl];
-							if(c) c.value = url;
-							lastImageCtrl = '';
-						} else {
-							return;
-						}
-					}
+				if (typeof(SetUrl) != 'function') {	
+							function SetUrl(url, width, height, alt) {
+								if(lastFileCtrl) {
+									var c = document.getElementById(lastFileCtrl);
+									if(c && c.value != url) {
+									    c.value = url;
+										SetUrlChange(c);
+									}
+									lastFileCtrl = '';
+								} else if(lastImageCtrl) {
+									var c = document.getElementById(lastImageCtrl);
+									if(c && c.value != url) {
+									    c.value = url;
+										SetUrlChange(c);
+									}
+									lastImageCtrl = '';
+								} else {
+									return;
+								}
+							}	
 				}	
  			");
  
@@ -69,13 +75,10 @@ function mm_widget_weblink($roles='', $templates='') {
 			$joutput = str_replace(chr(13), " ", $joutput); //remove carriage returns
 			
 			$output .= '$j("head").append(\' <script type="text/javascript">'.$joutput.'</scr\'+\'ipt> \'); ' . "\n";
-
-			//reset weblink field width to allow for insert button
-//			$output .= '$("input[@name=ta]").attr("style", "width:260px"); ' . "\n";
-			$output .= '$j("input[name=ta]").attr("style", "width:260px"); ' . "\n";
+			$output .= '$j("input[name=ta]").attr("id", "ta"); ' . "\n";
+			$output .= '$j("input[name=ta]").attr("style", "width:70%"); ' . "\n";
 	
 			// append insert button to weblink (ta)	
-//			$output .= '$("input[@name=ta]").after(\'';
 			$output .= '$j("input[name=ta]").after(\'';												   
 			$output .= '<input type="button" value="Insert" onclick="BrowseFileServer(\\\'ta\\\')">';					
 			$output .= '\');';
